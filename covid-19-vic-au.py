ec319af11f6d6ecfb05598e6391e6617a9c6cf04
Mike Honey <mike.honey@mangasolutions.com>
2020-07-10 09:18:26 +0000

    Add files via upload

--- covid-19-vic-au.py
+++ covid-19-vic-au.py
@@ -6,10 +6,10 @@ import pandas
 import subprocess
 import zipfile
     
-def processXlsx(dataurl, datadir):
+def processXlsx(datadir):
 
-    input_LGA_VIC_GDF = geopandas.read_file (dataurl + "LGA_2020_VIC.geojson")
-    input_VIC_AU_Sites_DF = pandas.read_excel (dataurl + "Coronavirus%20COVID-19%20-%20VIC%20AU%20Exposure%20sites.xlsx")
+    input_LGA_VIC_GDF = geopandas.read_file (datadir + "LGA_2020_VIC.geojson")
+    input_VIC_AU_Sites_DF = pandas.read_excel (datadir + "Coronavirus COVID-19 - VIC AU Exposure sites.xlsx")
     input_VIC_AU_Sites_GDF = geopandas.GeoDataFrame( input_VIC_AU_Sites_DF , 
         geometry=geopandas.points_from_xy(input_VIC_AU_Sites_DF.Longitude, input_VIC_AU_Sites_DF.Latitude))
     # derive a new geodata frame with the intersection of Basins and LGA shapes (constrained by Basins).
@@ -24,10 +24,9 @@ def main():
     Main - program execute
     """
     print (str(datetime.datetime.now()) + ' Starting ...')
-    dataurl = 'https://github.com/Mike-Honey/covid-19-vic-au/raw/master/'
     datadir = 'C:/Dev/covid-19-vic-au/'
 
-    processXlsx(dataurl, datadir)
+    processXlsx(datadir)
 
     print (str(datetime.datetime.now()) + ' Finished!')
     exit()

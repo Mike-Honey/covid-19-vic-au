6bbc34f90ccb6ab640bccc6eaa667a83ffec6d7d
Mike Honey <mike.honey@mangasolutions.com>
2020-12-31 00:18:07 +0000

    Add files via upload

--- covid-19-vic-au-get-exposure-sites.py
+++ covid-19-vic-au-get-exposure-sites.py
@@ -5,30 +5,36 @@ import pandas
 import requests
 
 
-def processWebPage(webpageURL, datadir, filename):
+def processWebPage(webpageURL, datadir, filename, table_instance, check_diff):
 
     html_page_df = pandas.read_html(webpageURL)
-    html_table0_df = html_page_df[0]
-    # print ( html_table0_df )
+    html_table_df = html_page_df[table_instance]
+    print ( html_table_df )
 
     # strip unicode characters
-    html_table0_cols = html_table0_df.select_dtypes(include=[numpy.object]).columns
-    html_table0_df[html_table0_cols] = html_table0_df[html_table0_cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+    html_table_cols = html_table_df.select_dtypes(include=[numpy.object]).columns.tolist()
+    html_table_df[html_table_cols] = html_table_df[html_table_cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+    html_table_df = html_table_df.sort_values(by=html_table_cols)
 
-    input_xlsx_df = pandas.read_excel(datadir + filename)
-    # print ( input_xlsx_df )
+    if check_diff:
+        input_xlsx_df = pandas.read_excel(datadir + filename)
+        # print ( input_xlsx_df )
 
-    diff_df = pandas.merge(input_xlsx_df, html_table0_df, how='outer', indicator='Exist')
+        diff_df = pandas.merge(input_xlsx_df, html_table_df, how='outer', indicator='Exist')
 
-    diff_df = diff_df.loc[diff_df['Exist'] != 'both']
-    print(diff_df.values)
+        diff_df = diff_df.loc[diff_df['Exist'] != 'both']
+        diff_df['Exist'] = diff_df['Exist'].replace('left_only', 'old')
+        diff_df['Exist'] = diff_df['Exist'].replace('right_only', 'new')
+        diff_df = diff_df.sort_values(by=diff_df.columns.tolist())
+        diff_file_name = datadir + str.replace(filename, '.xlsx', '_diff.xlsx')
+        if len(diff_df.index) > 0:
+            print(str(datetime.datetime.now()) + ' Differences found, wrote to file: ' + diff_file_name )
+            diff_df.to_excel (diff_file_name, index=False)
 
-    # assert_frame_equal(input_xlsx_df, html_table0_df, check_index_type=False)
-    # print ( input_xlsx_df.compare(html_table0_df) )
+    # write current data to excel file for next comparison run, also to timestamped file.
+    html_table_df.to_excel (datadir + filename, index=False)
+    html_table_df.to_excel (datadir + 'archive/' + str.replace(filename, '.xlsx', '_' + datetime.datetime.now().strftime('%y-%m-%d_%H_%M_%S') + '.xlsx'), index=False)
 
-    html_table0_df.to_excel (datadir + filename, index=False)
-
-    return diff_df
 
 def main():
     """
@@ -37,9 +43,11 @@ def main():
     print (str(datetime.datetime.now()) + ' Starting ...')
     webpageURL = 'https://www.dhhs.vic.gov.au/case-locations-and-outbreaks'
     datadir = 'C:/Dev/covid-19-vic-au/'
-    filename = 'case-locations-and-outbreaks#case-alerts--public-exposure-sites.xlsx'
+    filename = 'case-locations-and-outbreaks_case-alerts--public-exposure-sites.xlsx'
+    table_instance = 0
+    check_diff = True
 
-    dataset = processWebPage(webpageURL, datadir, filename)
+    processWebPage(webpageURL, datadir, filename, table_instance, check_diff)
 
     print (str(datetime.datetime.now()) + ' Finished!')
     exit()
